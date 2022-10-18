d827d99869aadb564acb30b8075ffea332839ae0
Miguel-Cabral <miguelanjo2@hotmail.com>
2022-10-18 11:23:01 +0100

    DOM exercise

--- A-dom-manipulation/exercise.js
+++ A-dom-manipulation/exercise.js
@@ -15,14 +15,37 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+const pElement = document.querySelectorAll("p");//exercise 1
+console.log(pElement);
 
 
+
+const firstDiv = document.querySelector("div");
+console.log(firstDiv);
+
+
+
+const elJumbotronText = document.querySelector("#jumbotron-text");
+console.log(elJumbotronText)
+
+
+
+const pElementInCountainer = document.querySelectorAll(".primary-content  p")
+console.log(pElementInCountainer)
 /*
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+const alertButton = document.querySelector("#alertBtn")
+alertButton.addEventListener("click", alertText);
+function alertText() {
+    alert("Thanks for visiting Bikes for Refugees!")
+}
+
+ 
+    
 
 
 /*
@@ -31,6 +54,15 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+const changeTextButton = document.querySelector("#bgrChangeBtn")
+const body = document.querySelector("body")
+changeTextButton.addEventListener("click", function() {
+
+   body.style.backgroundColor = "blue"
+})
+
+
+
 
 
 /*
@@ -39,7 +71,14 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
+const addText = document.querySelector("#addTextBtn")
+addText.addEventListener("click", newParagraph)
+function newParagraph() {
+ const newPara = document.querySelector(".jumbotron");
+ const paragraph = document.createElement("p")
+ paragraph.innerText = "Read more below"
+ newPara.appendChild(paragraph);
+}
 
 
 /*
@@ -48,3 +87,5 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+const largerLinks = document.querySelectorAll("#largerLinksBtn")
+largerLinks.addEventListener("click", )
